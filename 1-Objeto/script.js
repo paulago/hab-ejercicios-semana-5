1c8b8a855d4b81e4317b9805b01a25da8e3991d6
Paula Gomez <paula_gomez13@hotmail.com>
2022-04-24 01:09:14 +0200

    first ejer end

---
+++ 1-Objeto/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const people = {
+  Maria: 20,
+  Ana: 14,
+  Luis: 16,
+  Pepe: 35,
+  Manuel: 50,
+  Teresa: 12,
+  Daniel: 27,
+  Irene: 23,
+  Alex: 10,
+};
+
+function list(people) {
+  for (const [name, age] of Object.entries(people)) {
+    if (age >= 18) {
+      console.log(`${name} es mayor de edad`);
+    } else {
+      console.log(`${name} es menor de edad`);
+    }
+  }
+}
+
+list(people);
