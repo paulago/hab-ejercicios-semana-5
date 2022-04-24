4927c276e51a70f8e0e6f6ae3c22c107b8337120
Paula Gomez <paula_gomez13@hotmail.com>
2022-04-24 10:39:21 +0200

    ejer 4 finished

--- 3-funcion/script.js
+++ 3-funcion/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function password(num) {
+  const numUser = prompt("Introduce un número del 0 al 100");
+  let generatePassword = Math.random() * 100;
+
+  if (+numUser === generatePassword) {
+    prompt("¡Has ganado!");
+  } else {
+    prompt("El número es ");
+  }
+}
--- 4-bucles-anidados/script.js
+++ 4-bucles-anidados/script.js
@@ -8,3 +8,17 @@ const catalogo = [
   "barbacoa",
   "tropical",
 ];
+
+const getPizzaCombinations = (catalogue) => {
+  const pizzaCombinations = [];
+
+  for (let i = 0; i < catalogue.length; i++) {
+    for (let j = i + 1; j < catalogue.length; j++) {
+      pizzaCombinations.push(`${catalogue[i]} y ${catalogue[j]}`);
+    }
+  }
+
+  return pizzaCombinations;
+};
+
+getPizzaCombinations(catalogo);
