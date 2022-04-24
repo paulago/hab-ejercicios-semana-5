80e5bec7957cfc1cee5e709f2d2a62ae66b920be
Paula Gomez <paula_gomez13@hotmail.com>
2022-04-24 11:20:43 +0200

    ejer 3 finished

--- 3-funcion/script.js
+++ 3-funcion/script.js
@@ -1,12 +1,24 @@
 "use strict";
 
-function password(num) {
-  const numUser = prompt("Introduce un número del 0 al 100");
-  let generatePassword = Math.random() * 100;
+function password() {
+  const generatePassword = Math.floor(Math.random() * 101);
 
-  if (+numUser === generatePassword) {
-    prompt("¡Has ganado!");
-  } else {
-    prompt("El número es ");
+  for (let i = 0; i < 5; i++) {
+    const numUser = prompt("Introduce un número del 0 al 100");
+
+    if (+numUser === generatePassword) {
+      alert("¡Has ganado!");
+      break;
+    } else if (+numUser < generatePassword) {
+      alert(`El número es mayor. Te quedan ${4 - i} intentos.`);
+    } else {
+      alert(`El número es menor. Te quedan ${4 - i} intentos.`);
+    }
+
+    if (i === 4) {
+      alert("Has perdido! :(");
+    }
   }
 }
+
+password();
